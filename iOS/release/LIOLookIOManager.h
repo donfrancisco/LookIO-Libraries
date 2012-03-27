a9796ddba7378e2b26783d691ed0bee5e3122265
marc <marc@mc85472935.local>
2012-03-27 19:33:43 -0400

    Jenkins build v103

--- iOS/release/LIOLookIOManager.h
+++ iOS/release/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v102
+//  LookIO iOS Remote Support Client v103
 //  
 //  Copyright 2011-2012 LookIO, Inc. All rights reserved.
 //  
