67f6e887e15df20f5c5ae16e17335887b0da48f6
marc <marc@mc85472935.local>
2012-04-16 18:53:27 -0400

    Jenkins build v121

--- iOS/release/LIOLookIOManager.h
+++ iOS/release/LIOLookIOManager.h
@@ -1,6 +1,6 @@
 //  
 //  LIOLookIOManager.h
-//  LookIO iOS Remote Support Client v120
+//  LookIO iOS Remote Support Client v121
 //  
 //  Copyright 2011-2012 LookIO, Inc. All rights reserved.
 //  
@@ -9,7 +9,7 @@
 
 #import <UIKit/UIKit.h>
 
-#define LOOKIO_VERSION_STRING @"120"
+#define LOOKIO_VERSION_STRING @"121"
 
 @class LIOLookIOManager;
 
